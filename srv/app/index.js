9ae6fdb7cc3922500965149fff2f0bf4349d9755
tmcinerney <tmack.iv@gmail.com>
2018-01-22 14:55:21 -0600

    added https

--- srv/app/index.js
+++ srv/app/index.js
@@ -10,7 +10,7 @@ app.all(/.*/, function(req, res, next) {
     if (host.match(/^www\..*/i)) {
       next();
     } else {
-      res.redirect(301, "http://www." + host);
+      res.redirect(301, "https://www." + host);
     }
   });
   app.use(express.static(__dirname + "/public"));
@@ -42,7 +42,7 @@ app.use(function (req, res, next) {
 
 });
 app.get('*', function(req, res) {
-    res.redirect('https://' + req.headers.host + req.url);
+    res.redirect('https://www.' + req.headers.host + req.url);
 
     // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
     // res.redirect('https://example.com' + req.url);
